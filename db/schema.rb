8df293aeb5b58ba091ebb469696e52b98f6d838c
SwapnilPatil <swapnil.patil04@gmail.com>
2017-03-03 16:38:23 +0530

    work on UI

--- db/schema.rb
+++ db/schema.rb
@@ -13,98 +13,101 @@
 
 ActiveRecord::Schema.define(version: 20170303034701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cohabitant_addresses", force: :cascade do |t|
-    t.integer  "cohabitant_id", limit: 4
-    t.string   "address",       limit: 255
-    t.string   "email",         limit: 255
-    t.string   "mobile_1",      limit: 255
-    t.string   "mobile_2",      limit: 255
-    t.string   "mobile_3",      limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.integer  "cohabitant_id"
+    t.string   "address"
+    t.string   "email"
+    t.string   "mobile_1"
+    t.string   "mobile_2"
+    t.string   "mobile_3"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "cohabitant_educations", force: :cascade do |t|
-    t.integer  "cohabitant_id",   limit: 4
-    t.string   "area",            limit: 255
-    t.string   "education",       limit: 255
-    t.string   "occupation",      limit: 255
-    t.string   "occupation_city", limit: 255
-    t.float    "income",          limit: 24
-    t.boolean  "is_income_annua", limit: 1,   default: true
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.integer  "cohabitant_id"
+    t.string   "area"
+    t.string   "education"
+    t.string   "occupation"
+    t.string   "occupation_city"
+    t.float    "income"
+    t.boolean  "is_income_annua", default: true
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   create_table "cohabitant_expectations", force: :cascade do |t|
-    t.integer  "cohabitant_id",  limit: 4
-    t.string   "cities",         limit: 255
-    t.string   "mangal",         limit: 255
-    t.string   "caste",          limit: 255
-    t.string   "age_difference", limit: 255
-    t.string   "min_height",     limit: 255
-    t.boolean  "divorcee",       limit: 1
-    t.string   "occupation",     limit: 255
-    t.string   "education",      limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.integer  "cohabitant_id"
+    t.string   "cities"
+    t.string   "mangal"
+    t.string   "caste"
+    t.string   "age_difference"
+    t.string   "min_height"
+    t.boolean  "divorcee"
+    t.string   "occupation"
+    t.string   "education"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "cohabitant_families", force: :cascade do |t|
-    t.integer  "cohabitant_id",         limit: 4
-    t.boolean  "has_mother",            limit: 1
-    t.string   "mother_name",           limit: 255
-    t.boolean  "has_father",            limit: 1
-    t.string   "father_name",           limit: 255
-    t.integer  "brothers",              limit: 4
-    t.integer  "married_brothers",      limit: 4
-    t.integer  "sisters",               limit: 4
-    t.integer  "married_sisters",       limit: 4
-    t.string   "parents_occupation",    limit: 255
-    t.string   "surnames_of_relatives", limit: 255
-    t.string   "native_district",       limit: 255
-    t.string   "native_taluka",         limit: 255
-    t.string   "family_wealth",         limit: 255
-    t.string   "mamas_name",            limit: 255
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.integer  "cohabitant_id"
+    t.boolean  "has_mother"
+    t.string   "mother_name"
+    t.boolean  "has_father"
+    t.string   "father_name"
+    t.integer  "brothers"
+    t.integer  "married_brothers"
+    t.integer  "sisters"
+    t.integer  "married_sisters"
+    t.string   "parents_occupation"
+    t.string   "surnames_of_relatives"
+    t.string   "native_district"
+    t.string   "native_taluka"
+    t.string   "family_wealth"
+    t.string   "mamas_name"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "cohabitant_horoscops", force: :cascade do |t|
-    t.string   "rashi",         limit: 255
-    t.string   "nakshtra",      limit: 255
-    t.string   "charan",        limit: 255
-    t.string   "nadi",          limit: 255
-    t.string   "gan",           limit: 255
-    t.string   "birth_place",   limit: 255
+    t.string   "rashi"
+    t.string   "nakshtra"
+    t.string   "charan"
+    t.string   "nadi"
+    t.string   "gan"
+    t.string   "birth_place"
     t.datetime "birth_time"
-    t.boolean  "mangal",        limit: 1
-    t.string   "devak",         limit: 255
-    t.integer  "cohabitant_id", limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.boolean  "mangal"
+    t.string   "devak"
+    t.integer  "cohabitant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "cohabitants", force: :cascade do |t|
-    t.string   "first_name",               limit: 255, null: false
-    t.string   "last_name",                limit: 255, null: false
-    t.string   "middle_name",              limit: 255, null: false
-    t.date     "date_of_birth",                        null: false
-    t.string   "sub_cast",                 limit: 255
-    t.string   "married_status",           limit: 255
-    t.float    "height",                   limit: 24
-    t.float    "weight",                   limit: 24
-    t.string   "blood_group",              limit: 255
-    t.string   "complexion",               limit: 255
-    t.boolean  "is_physical_disabilities", limit: 1
-    t.string   "physical_disabilities",    limit: 255
-    t.string   "diet",                     limit: 255
-    t.boolean  "spectacles",               limit: 1
-    t.boolean  "lens",                     limit: 1
-    t.string   "mobile",                   limit: 255
-    t.string   "reg_id",                   limit: 255
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.string   "first_name",               null: false
+    t.string   "last_name",                null: false
+    t.string   "middle_name",              null: false
+    t.date     "date_of_birth",            null: false
+    t.string   "sub_cast"
+    t.string   "married_status"
+    t.float    "height"
+    t.float    "weight"
+    t.string   "blood_group"
+    t.string   "complexion"
+    t.boolean  "is_physical_disabilities"
+    t.string   "physical_disabilities"
+    t.string   "diet"
+    t.boolean  "spectacles"
+    t.boolean  "lens"
+    t.string   "mobile"
+    t.string   "reg_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
